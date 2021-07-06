c95686d519cbee898895845f78687a1f1ec1f093
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-07-07 00:00:12 +0530

    [Anjaneya]Added:Welcome message for product review management

---
+++ ProductManagement/ProductManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management system!");
+        }
+    }
+}
